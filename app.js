7bcfb2864763309c3860c7f26fd68f95891c73ba
tibraj <tibraj94@gmail.com>
2021-01-14 14:34:50 -0500

    maximum subarray

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 //Maximum of Sub Array
-
+function subArray(arr) {
+    let a1 = 0;
+    let a2 = arr[0];
+    arr.forEach((i, a) => {
+        a1 = Math.max(i, a1 + i);
+        a2 = Math.max(a2, a1);
+    })
+    return a2;
+}
 
 //Plus One
 
