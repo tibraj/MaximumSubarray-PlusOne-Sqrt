1903f420fd80c649b8be311707b2bce557447e0a
tibraj <tibraj94@gmail.com>
2021-01-14 14:37:32 -0500

    plus one

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ console.log(subArray([-2,1,-3,4,-1,2,1,-5,4]));
 
 //Plus One
 
+function plusOne(digits) {
+    let i = digits.length -1;
+    let val = 0;
+    let carry = 1;
+    while(i >= 0 && carry) {
+        val = digits[i] + carry;
+        carry = Math.floor(val / 10);
+        digits[i] = val % 10;
+        i--;
+    }
+    if(carry) digits.unshift(carry);
+    return digits;
+}
+
 
 
 //Sqrt
